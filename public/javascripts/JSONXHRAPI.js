a675609e41f0dd2cd22156715c2730f476bad7c6
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-11-16 15:48:06 +0100

    done

--- public/javascripts/JSONXHRAPI.js
+++ public/javascripts/JSONXHRAPI.js
@@ -1,1 +1,23 @@
-console.log("is it alive?");
+//XHR 
+var XHR = new XMLHttpRequest();
+var NoteJSON;
+
+XHR.onreadystatechange = function () {
+    if (this.status == 200 && this.readyState == 4) {
+        NoteJSON = JSON.parse(this.responseText);
+    }
+};
+
+XHR.open("GET", "/users", true);
+
+XHR.send();
+
+function WriteData(){
+    var htmlData = "";
+
+    for (var i = 0; i < NoteJSON.length; i++) {
+        htmlData += "<li><b>" + NoteJSON[i].NoteTitle + "</b></br>" + NoteJSON[i].NoteContent + "</li></br>";  
+    }
+
+    document.getElementById("displayData").innerHTML = htmlData;
+};
